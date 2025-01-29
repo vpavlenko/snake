3500b57d7c449f2ff33694b46959c76b2de73c16
Vitaly Pavlenko <cxielamiko@gmail.com>
2025-01-29 19:29:33 +0100

    Fix dependencies and base URL

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./", // Change to relative path for GitHub Pages
+  base: "/snake/", // Match your repository name
 });
